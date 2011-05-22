b10aa11b4c91520e590976a189c37e745abe00ce
Daniel Ratiu <ratiud@googlemail.com>
2011-05-22 22:12:59 +0200

    improvements in displaying the Scala comments from sources

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaHover.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaHover.scala
@@ -14,41 +14,44 @@ import scala.tools.nsc.symtab.Flags
 import org.eclipse.jdt.internal.core.JavaModelManager
 import scala.tools.eclipse.contribution.weaving.jdt.IScalaElement
 import scala.tools.eclipse.javaelements.ScalaSourceTypeElement
+import org.eclipse.jface.internal.text.html.HTMLPrinter
+import org.eclipse.jdt.internal.ui.text.java.hover.JavadocBrowserInformationControlInput
+import util.ReflectionUtils
+import org.eclipse.jdt.internal.ui.JavaPlugin
+import org.eclipse.jdt.internal.core.SourceType
+import scala.tools.nsc.doc.model.comment.CommentFactory
+import scala.tools.nsc.doc.model.ModelFactory
+import scala.tools.nsc.doc.model.TreeFactory
+import scala.tools.nsc.doc.html.HtmlPage
+import scala.xml.NodeSeq
 
-class ScalaHover(codeAssist: Option[ICodeAssist]) extends JavadocHover {
+class ScalaHover(codeAssist: Option[ICodeAssist]) extends JavadocHover with ReflectionUtils {
   
-  private val NoHoverInfo = "" // could return null, but prefer to return empty (see API of ITextHover).
-
   override def getJavaElementsAt(textViewer: ITextViewer, region: IRegion) = {
     codeAssist match {
       case Some(scu: ScalaCompilationUnit) => {
         val start = region.getOffset
         val end = start + region.getLength
         scu.withSourceFile({ (src, compiler) =>
-          import compiler._
-          def hoverInfo(t: Tree): Option[IMember] = askOption { () =>
-            val element = compiler.getJavaElement(t.symbol)
-            if (element.nonEmpty) {
-              element.get
-            } else {
-              null
-            }
-          }
+          import compiler._        
           val resp = new Response[Tree]
           val range = compiler.rangePos(src, start, start, end)
           askTypeAt(range, resp)
-          var element: Option[IMember] = None
-          if (resp.get.isLeft) {
-            element = hoverInfo(resp.get.left.get)
-          }
-          if (element.nonEmpty && (!element.isInstanceOf[IScalaElement])) {
-            Array[IJavaElement](element.get)
-          } else {
-            Array[IJavaElement]()
-          }
-        })(Array[IJavaElement]())
+          resp.get match {
+            case Left(tree) => 
+              askOption { () => 
+                compiler.getJavaElement(tree.symbol).getOrElse(null) 
+              } match {
+                case Some(el) => Array[IJavaElement](el)                  	
+                case _ => Array.empty[IJavaElement]
+              }
+            case _ => 
+              Array.empty[IJavaElement]
+          }          
+        })(Array.empty[IJavaElement])
       }
-      case _ => Array[IJavaElement]()
+      case _ => 
+        Array.empty[IJavaElement]
     }
   }
 
@@ -58,47 +61,80 @@ class ScalaHover(codeAssist: Option[ICodeAssist]) extends JavadocHover {
       try {
         super.getHoverInfo2(textViewer, hoverRegion)
       } catch {
-        case e => getHoverInfo(textViewer, hoverRegion)
+        case e => buildScalaHover(textViewer, hoverRegion)
       }
     } else {
-      getHoverInfo(textViewer, hoverRegion)
+      buildScalaHover(textViewer, hoverRegion)
     }
   }
-
-  override def getHoverInfo(viewer: ITextViewer, region: IRegion) = {
+  
+  val classImage = JavaPlugin.getDefault().getImagesOnFSRegistry().getImageURL(ScalaImages.SCALA_CLASS)
+  val objectImage = JavaPlugin.getDefault().getImagesOnFSRegistry().getImageURL(ScalaImages.SCALA_OBJECT)
+  def buildScalaHover(viewer: ITextViewer, region: IRegion) = {
+    val buffer= new StringBuffer();    
+    val javadocHoverClazz = Class.forName("org.eclipse.jdt.internal.ui.text.java.hover.JavadocHover")
+    val getStyleSheetMethod = getDeclaredMethod(javadocHoverClazz, "getStyleSheet") 
+    HTMLPrinter.insertPageProlog(buffer, 0, getStyleSheetMethod.invoke(null).asInstanceOf[String]);
+    
     codeAssist match {
       case Some(scu: ScalaCompilationUnit) => {
         val start = region.getOffset
         val end = start + region.getLength
         scu.withSourceFile({ (src, compiler) =>
           import compiler._
-          def hoverInfo(t: Tree): Option[String] = askOption { () =>
-            def compose(ss: List[String]): String = ss.filter("" !=).mkString("", " ", "")
-            def defString(sym: Symbol, tpe: Type): String = {
-              // NoType is returned for defining occurrences, in this case we want to display symbol info itself. 
-              val tpeinfo = if (tpe ne NoType) tpe.widen else sym.info
-              compose(List(sym.hasFlagsToString(Flags.ExplicitFlags), sym.keyString,
-                sym.varianceString + sym.nameString + sym.infoString(tpeinfo)))
-            }
+          def doBuildHover(t: Tree) { 
+            askOption { () =>          
+              def compose(ss: List[String]): String = ss.filter("" !=).mkString("", " ", "")
+              def defString(sym: Symbol, tpe: Type): String = {
+                // NoType is returned for defining occurrences, in this case we want to display symbol info itself. 
+                val tpeinfo = if (tpe ne NoType) tpe.widen else sym.info
+                compose(List(sym.hasFlagsToString(Flags.ExplicitFlags), sym.keyString,
+                  sym.varianceString + sym.nameString + sym.infoString(tpeinfo)))
+              }
             
-            for (sym <- Option(t.symbol); tpe <- Option(t.tpe)) 
-              yield {
-               val com = rawDocComment(sym) 
-               val doc = toJavaDoc(rawDocComment(sym));
-                if (!doc.equals("")) doc
-                else if (sym.isClass || sym.isModule) sym.fullName 
-                else defString(sym, tpe)                     
-              }                        
-          } getOrElse None
+              import scala.tools.nsc.doc.Settings
+              object MyCommentFactory extends ModelFactory(compiler, new Settings({ e : String => })) 
+              	with CommentFactory with TreeFactory {              	                 			    
+                
+                def scalaDocComment2Html(expanded : String, raw : String, pos : Position) : String = {
+                  val comment = parse(expanded, raw, pos)                  
+                  val htmlConvertor = new HtmlPage {
+                    val path = List("")
+                    val title = ""  
+	  		        val headers = NodeSeq.Empty
+			        val body = NodeSeq.Empty			        
+			        def getHtmlAsString = bodyToHtml(comment.body).toString
+                  }                  
+                  htmlConvertor.getHtmlAsString
+                }
+              }
+                            
+              for (sym <- Option(t.symbol); tpe <- Option(t.tpe)) 
+                yield {                
+                  if (sym.isClass || sym.isModule) { 
+                    val image = if (sym.isClass) classImage.toExternalForm 
+                    			else objectImage.toExternalForm 
+                    JavadocHover.addImageAndLabel(buffer, image, 
+                        16, 16, sym.fullName, 20, 2);                                             
+                  } else 
+                    HTMLPrinter.addSmallHeader(buffer, defString(sym, tpe))  
+                  val commentAsHtml = MyCommentFactory.scalaDocComment2Html(expandedDocComment(sym), rawDocComment(sym), sym.pos)
+                  buffer.append(commentAsHtml);                                                        
+                }                        
+            }
+          }
           
           val resp = new Response[Tree]
           val range = compiler.rangePos(src, start, start, end)
           askTypeAt(range, resp)
-          (for (t <- resp.get.left.toOption; hover <- hoverInfo(t)) yield hover) getOrElse NoHoverInfo
-        })(NoHoverInfo)
+          resp.get.left.foreach(doBuildHover(_))          
+        })()
       }
-      case _ => NoHoverInfo
+      case _ => 
     }
+     
+	HTMLPrinter.addPageEpilog(buffer);
+	new JavadocBrowserInformationControlInput(null, null, buffer.toString, 20)
   }
 
   override def getHoverRegion(viewer: ITextViewer, offset: Int) = {
